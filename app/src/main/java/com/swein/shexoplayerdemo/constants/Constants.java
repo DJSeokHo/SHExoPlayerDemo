a06d7b6038aee1b510e44b3af73421930b087ca5
DJSeokHo <djseokho@gmail.com>
2020-10-27 19:27:34 +0900

    update gradle

--- app/src/main/java/com/swein/shexoplayerdemo/constants/Constants.java
+++ app/src/main/java/com/swein/shexoplayerdemo/constants/Constants.java
@@ -7,7 +7,9 @@ public class Constants {
     public final static String RTMP_URL = "rtmp://haneultv.xst.kinxcdn.com/haneultv/pBnf4x";
 
     // vod mp4 测试地址，可能需要翻墙，不翻墙请自行寻找
-    public final static String MP4_VOD_URL = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
+//    public final static String MP4_VOD_URL = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
+//    public final static String MP4_VOD_URL = "https://dl.dropbox.com/s/i98qwq7s8ou1c0i/KakaoTalk_Video_2020-09-14-17-13-35.mp4";
+    public final static String MP4_VOD_URL = "https://dl.dropbox.com/s/i98qwq7s8ou1c0i/KakaoTalk_Video_2020-09-14-17-13-35.mp4";
     public final static String HLS_VOD_URL = "";
 
     // 播放器我定义的默认为16：9的画面，可以自由修改
